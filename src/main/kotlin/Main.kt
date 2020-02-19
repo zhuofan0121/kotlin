bec513605396414929498f6a9b539e10975aa7e3
jiazhuofan <zhuofan7@illinois.edu>
2020-02-19 14:30:42 -0600

    Kotlin WebApi

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,22 @@
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
 fun hello(): String {
     return "Hello World!"
 }
 fun add(first: Int, second: Int): Int {
     return first + second
+}
+fun main() {
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello World!")
+            }
+        }
+    }.start(wait = true)
 }
